0b31707b927c2cb2dfec28b9c3e481a24533b1e5
Moaz Elseyoufi <71258953+moazie@users.noreply.github.com>
2023-09-14 12:18:50 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+This is the intellectual property of Moaz Elseyoufi 2023
+Protected By  GNU GENERAL PUBLIC LICENSE
+*/
+
+let username = window.prompt("Enter Value Username:");
+document.getElementById("p1").innerHTML = "Hello, " + username
+console.log("Hello, " + username);
